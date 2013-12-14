9dacd96d5f7a8fad7c029b2566d3fec2eb7a2a29
Falko Benthin <f.benthin@jpberlin.de>
2013-12-14 23:26:22 +0100

    bug in helpers/check-internet-connection.sh fixed

--- helpers/check-internet-connection.sh
+++ helpers/check-internet-connection.sh
@@ -7,7 +7,7 @@ URLS=('www.kernel.org' 'www.debian.org' 'www.archlinux.org');
 COUNT=0
 for (( i=0; i<${#URLS[@]}; i++ ))
 do
-	ping -c 1 ${URLS[$i]} > /dev/null
+	ping -W2 -c1 ${URLS[$i]} > /dev/null
 	if [ "$?" -eq 0 ]
 	then
 		let "COUNT += 1"
@@ -17,5 +17,5 @@ done
 if [ $COUNT -eq 0 ]
 then
 	PATH_TO_SEHEIAH="/home/falko/seheiah"
-	mpg321 -o alsa -a hw:0,0 $PATH_TO_SEHEIAH/audiofiles/de_lost_internet_connection.mp3.mp3
+	mpg321 -o alsa -a hw:0,0 $PATH_TO_SEHEIAH/audiofiles/de_lost_internet_connection.mp3
 fi
