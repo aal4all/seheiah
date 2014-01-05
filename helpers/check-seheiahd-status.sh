06669dfd082654984c47aa13a6eae54e97eb4849
Falko Benthin <f.benthin@jpberlin.de>
2014-01-05 02:18:56 +0100

    added pigpiod to seheiah start script

--- helpers/check-seheiahd-status.sh
+++ helpers/check-seheiahd-status.sh
@@ -2,12 +2,31 @@
 # checks, if seheiahd is running
 # you shuould run it in cronjob
 #!/bin/bash
+
 if [ `ps -e | grep -c "seheiahd.py" ` -eq 0 ]
 then
 	echo "Daemon ist gestorben" | /usr/bin/mail -s "Daemon ist tot" mail@address
 	sleep 10
 	PATH_TO_SEHEIAH="/home/falko/seheiah"
 	mpg321 -o alsa -a hw:0,0 $PATH_TO_SEHEIAH/audiofiles/de_seheiahd_isnt_running.mp3
+	
+	#check for pigpiod
+	#if pir activated
+	if [ `grep -c "pir = true" $PATH_TO_SEHEIAH/program/seheiah.cfg` -eq 1 ]
+	then
+		#check if prog exists
+		if [ -f /usr/local/bin/pigpiod ]
+		then
+			#and start it if isn't running
+			if [ `ps -e | grep -c "pigpiod" ` -eq 0 ]
+			then
+				sudo pigpiod
+			fi
+		else
+			echo "ERROR: install pigpiod" | tee $PATH_TO_SEHEIAH/logs/seheiah.log
+			exit 1
+		fi
+	fi
 	
   #starts seheiah in screen session, otherwise it doesn't works in cronjob
 	if [ `screen -ls | grep -c "screen-seheiah" ` -eq 0 ]
@@ -27,6 +46,4 @@ then
     "	
 	fi
 fi
-echo $?
 
-/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
